8ce4da2cdac5caa2f91f5b9f60c3427b62a33e07
Vladimir <dr.frisbi@gmail.com>
2022-12-20 22:20:17 +0700

    nahodim kvadrat

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// int number = Int.Parse(Console.ReadLine())
+int sqr = number * number;
+Console.Write("Квадрат числа: ");
+Console.WriteLine(sqr);
