5639f077497c7df423a698364e6f12f6fb55425b
Vladimir <dr.frisbi@gmail.com>
2022-12-20 23:06:53 +0700

    добавили квадрат числа 1 и 2

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,11 +1,23 @@
-﻿Console.Write("Введите число: ");
+﻿// Console.Write("Введите число: ");
 
-int number = Convert.ToInt32(Console.ReadLine());
+// int number = Convert.ToInt32(Console.ReadLine());
 
-// int number = Int.Parse(Console.ReadLine())
+// // int number = Int.Parse(Console.ReadLine())
 
 
-// int sqr = number * number;
+// // int sqr = number * number;
 
-// Console.Write($"Квадрат числа {number} равен: {sqr}");
-Console.Write($"Квадрат числа {number} равен: {number * number}");
+// // Console.Write($"Квадрат числа {number} равен: {sqr}");
+// Console.Write($"Квадрат числа {number} равен: {number * number}");
+
+
+Console.Write("Введите первое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == numberB * numberB)
+   Console.Write($"Число {numberA} является квадратом {numberB}"); 
+else
+    Console.Write($"Число {numberA} не является квадратом {numberB}");
